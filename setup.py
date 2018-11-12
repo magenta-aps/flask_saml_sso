f3a76813c683f092a300b4e1781c2fa58b6ff587
Christoffer Moesgaard <christoffer@magenta.dk>
2018-11-12 12:57:22 +0100

    Bumped version to 0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages
 
 setup(
     name='flask_saml_sso',
-    version='0.0.1',
+    version='0.1.0',
     description='',
     author='Magenta ApS',
     author_email='info@magenta.dk',
