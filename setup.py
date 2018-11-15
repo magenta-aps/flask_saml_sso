a061107b000b75bf5294afd33b0153f43a549b82
Christoffer Moesgaard <christoffer@magenta.dk>
2018-11-15 09:19:06 +0100

    More consistent use of cookies and headers. Headers are now always checked before cookies

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages
 
 setup(
     name='flask_saml_sso',
-    version='0.1.1',
+    version='0.1.2',
     description='',
     author='Magenta ApS',
     author_email='info@magenta.dk',
