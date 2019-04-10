7510a7ee44d726b5948aace884c668097a583a7e
Christoffer Moesgaard <christoffer@magenta.dk>
2019-04-10 13:33:59 +0200

    Bump version to 0.5.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import find_packages
 
 setup(
     name='flask_saml_sso',
-    version='0.5.0',
+    version='0.5.1',
     description='',
     author='Magenta ApS',
     author_email='info@magenta.dk',
